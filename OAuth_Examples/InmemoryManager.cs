6fb9f15df5680950e3ed2be38e29af3d424b7ad5
Raveendranath Jonna <rjonna@ciqhyd.com>
2017-06-30 16:45:01 +0530

    First commit

---
+++ OAuth_Examples/InmemoryManager.cs
@@ -0,0 +1,58 @@
+﻿using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace OAuth_Examples
+{
+    public class InmemoryManager
+    {
+        public List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>()
+            {
+                new InMemoryUser() {Subject ="XYZ", Username = "XYZ.gmail.com",Password="123", Claims=new[] { new Claim(Constants.ClaimTypes.Name,"xyz") } },
+                          new InMemoryUser() {Subject ="abc", Username = "abc.gmail.com",Password="123", Claims=new[] { new Claim(Constants.ClaimTypes.Name,"abc") } },
+                          new InMemoryUser() {Subject ="pqr", Username = "pqr.gmail.com",Password="123", Claims=new[] { new Claim(Constants.ClaimTypes.Name,"pqr") } }
+            };
+        }
+
+        public IEnumerable<Scope> GetScopes()
+        {
+            return new[]
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile,
+                StandardScopes.OfflineAccess,
+                new Scope {Name ="Read", DisplayName ="Read only Data" }
+            };
+        }
+
+
+        public IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId ="ClientID1",
+                    ClientSecrets = new List<Secret> {
+                                            new Secret("Secret".Sha256())
+                                           },
+                    ClientName ="ClientName1",
+                    Flow=Flows.ResourceOwner,
+                    AllowedScopes = new List<string>
+                    {
+                        Constants.StandardScopes.OpenId,
+                        "read"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ OAuth_Examples/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.Core.Configuration;
+using System.Security.Cryptography.X509Certificates;
+
+[assembly: OwinStartup(typeof(OAuth_Examples.Startup))]
+
+namespace OAuth_Examples
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            InmemoryManager inMemMgr = new InmemoryManager();         
+            var factory = new IdentityServerServiceFactory()
+                .UseInMemoryUsers(inMemMgr.GetUsers())
+                .UseInMemoryScopes(inMemMgr.GetScopes())
+                .UseInMemoryClients(inMemMgr.GetClients());
+            string Certificate = "C:\\Raveendra\\Tools\\RJCert.pfx";    
+            var cert2 = new X509Certificate2(Certificate, "password");
+            var options = new IdentityServerOptions
+            {             
+                SigningCertificate = cert2,
+                RequireSsl = false,
+                Factory = factory
+            };
+            app.UseIdentityServer(options);
+        }
+    }
+}
